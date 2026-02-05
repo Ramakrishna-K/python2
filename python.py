486b1be6509f8b53533331891162cbe00229322e
Ramakrishna-K <ramakrishnakatravth0@gmail.com>
2026-02-05 20:02:21 +0530

    python2

---
+++ python.py
@@ -0,0 +1,30 @@
+
+# for i in range(1, 16):
+#     if i % 3 == 0 and i % 5 == 0:  
+#         print("FizzBuzz")
+#     elif i % 3 == 0:
+#         print("Fizz")
+#     elif i % 5 == 0:
+#         print("Buzz")
+#     else:
+#         print(i)
+
+
+# N = 5;
+# sum = 0
+
+# for i in range(1 , N+1):
+#     sum = sum + i
+# print(sum)
+
+
+# for i in range(1, 21):
+#     if i%2 == 0:
+#         print(i)   
+
+for i in range(1, 11):
+    print("2 *",i,"=", 2 * i)
+        
+
+
+
